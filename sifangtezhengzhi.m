e6efd5d3aca5bc1b4f579ad31a40a15759a457f5
weizhen96 <weizhen9612@163.com>
2026-03-30 20:47:21 +0800

    Update sifangtezhengzhi.m

--- sifangtezhengzhi.m
+++ sifangtezhengzhi.m
@@ -1,145 +1,143 @@
-%均衡点E1（找R）
 clc;clear;
-syms x y z w C1 C2 C3 C4 V a b E1 E2 Fa Rd Rg L Fi Ri T;%加粗变量自行更改
-x=0,y=0,z=0,w=0; %均衡点E1
+syms x y z w C1 C2 C3 C4 V a b E1 E2 Fa Rd Rg L Fi Ri T;
+x=0,y=0,z=0,w=0; 
 J = [(1-2*x)*(-C1-V*a+V*b+2*V*a*y-V*b*y-Fa*y*z) x*(1-x)*(2*V*a-V*b-Fa*z) x*(1-x)*(-Fa*y) 0;
     y*(1-y)*(Rd-2*V*a+V*b) (1-2*y)*(-C2-Rd+V*a+Rd*x-2*V*a*x+V*b*x) 0 0;
     z*(1-z)*(Fa*y+L*y+Fi*w*y+Ri*w*y) z*(1-z)*(- Fa- L- Fi*w+ Fa*x+ L*x- Ri*w+ Fi*w*x+ Ri*w*x) (1-2*z)*(Fa-C3+L+Fi*w - Fa*y - L*y + Ri*w - Fi*w*y + Fa*x*y + L*x*y - Ri*w*y + Fi*w*x*y + Ri*w*x*y) z*(1-z)*(Fi+Ri- Fi*y - Ri*y+ Fi*x*y+Ri*x*y)
-    w*(1-w)*(C4 - Fi - T + Fi*z + Ri*z + T*z)*(-y) w*(1-w)*(C4 - Fi - T + Fi*z + Ri*z + T*z)*(1-x) w*(1-w)*(y-1-x*y)*(Fi + Ri + T) (1-2*w)*(y-1-x*y)*(C4 - Fi - T + Fi*z + Ri*z + T*z)];%雅克比矩阵
+    w*(1-w)*(C4 - Fi - T + Fi*z + Ri*z + T*z)*(-y) w*(1-w)*(C4 - Fi - T + Fi*z + Ri*z + T*z)*(1-x) w*(1-w)*(y-1-x*y)*(Fi + Ri + T) (1-2*w)*(y-1-x*y)*(C4 - Fi - T + Fi*z + Ri*z + T*z)];
 [V,R] = eig(J),DA=det(J),IA=inv(J),
-%其他均衡点同理
-%均衡点E2（找R）
+
 clc;clear;
-syms x y z w C1 C2 C3 C4 V a b E1 E2 Fa Rd Rg L Fi Ri T;%加粗变量自行更改
-x=1,y=0,z=0,w=0; %均衡点E2
+syms x y z w C1 C2 C3 C4 V a b E1 E2 Fa Rd Rg L Fi Ri T;
+x=1,y=0,z=0,w=0; 
 J = [(1-2*x)*(-C1-V*a+V*b+2*V*a*y-V*b*y-Fa*y*z) x*(1-x)*(2*V*a-V*b-Fa*z) x*(1-x)*(-Fa*y) 0;
     y*(1-y)*(Rd-2*V*a+V*b) (1-2*y)*(-C2-Rd+V*a+Rd*x-2*V*a*x+V*b*x) 0 0;
     z*(1-z)*(Fa*y+L*y+Fi*w*y+Ri*w*y) z*(1-z)*(- Fa- L- Fi*w+ Fa*x+ L*x- Ri*w+ Fi*w*x+ Ri*w*x) (1-2*z)*(Fa-C3+L+Fi*w - Fa*y - L*y + Ri*w - Fi*w*y + Fa*x*y + L*x*y - Ri*w*y + Fi*w*x*y + Ri*w*x*y) z*(1-z)*(Fi+Ri- Fi*y - Ri*y+ Fi*x*y+Ri*x*y)
-    w*(1-w)*(C4 - Fi - T + Fi*z + Ri*z + T*z)*(-y) w*(1-w)*(C4 - Fi - T + Fi*z + Ri*z + T*z)*(1-x) w*(1-w)*(y-1-x*y)*(Fi + Ri + T) (1-2*w)*(y-1-x*y)*(C4 - Fi - T + Fi*z + Ri*z + T*z)];%雅克比矩阵
+    w*(1-w)*(C4 - Fi - T + Fi*z + Ri*z + T*z)*(-y) w*(1-w)*(C4 - Fi - T + Fi*z + Ri*z + T*z)*(1-x) w*(1-w)*(y-1-x*y)*(Fi + Ri + T) (1-2*w)*(y-1-x*y)*(C4 - Fi - T + Fi*z + Ri*z + T*z)];% 
 [V,R] = eig(J),DA=det(J),IA=inv(J),
-%均衡点E3（找R）
+
 clc;clear;
-syms x y z w C1 C2 C3 C4 V a b E1 E2 Fa Rd Rg L Fi Ri T;%加粗变量自行更改
-x=0,y=1,z=0,w=0; %均衡点E3
+syms x y z w C1 C2 C3 C4 V a b E1 E2 Fa Rd Rg L Fi Ri T;
+x=0,y=1,z=0,w=0; 
 J = [(1-2*x)*(-C1-V*a+V*b+2*V*a*y-V*b*y-Fa*y*z) x*(1-x)*(2*V*a-V*b-Fa*z) x*(1-x)*(-Fa*y) 0;
     y*(1-y)*(Rd-2*V*a+V*b) (1-2*y)*(-C2-Rd+V*a+Rd*x-2*V*a*x+V*b*x) 0 0;
     z*(1-z)*(Fa*y+L*y+Fi*w*y+Ri*w*y) z*(1-z)*(- Fa- L- Fi*w+ Fa*x+ L*x- Ri*w+ Fi*w*x+ Ri*w*x) (1-2*z)*(Fa-C3+L+Fi*w - Fa*y - L*y + Ri*w - Fi*w*y + Fa*x*y + L*x*y - Ri*w*y + Fi*w*x*y + Ri*w*x*y) z*(1-z)*(Fi+Ri- Fi*y - Ri*y+ Fi*x*y+Ri*x*y)
-    w*(1-w)*(C4 - Fi - T + Fi*z + Ri*z + T*z)*(-y) w*(1-w)*(C4 - Fi - T + Fi*z + Ri*z + T*z)*(1-x) w*(1-w)*(y-1-x*y)*(Fi + Ri + T) (1-2*w)*(y-1-x*y)*(C4 - Fi - T + Fi*z + Ri*z + T*z)];%雅克比矩阵
+    w*(1-w)*(C4 - Fi - T + Fi*z + Ri*z + T*z)*(-y) w*(1-w)*(C4 - Fi - T + Fi*z + Ri*z + T*z)*(1-x) w*(1-w)*(y-1-x*y)*(Fi + Ri + T) (1-2*w)*(y-1-x*y)*(C4 - Fi - T + Fi*z + Ri*z + T*z)];
 [V,R] = eig(J),DA=det(J),IA=inv(J),
-%均衡点E4（找R）
+
 clc;clear;
-syms x y z w C1 C2 C3 C4 V a b E1 E2 Fa Rd Rg L Fi Ri T;%加粗变量自行更改
-x=0,y=0,z=1,w=0; %均衡点E4
+syms x y z w C1 C2 C3 C4 V a b E1 E2 Fa Rd Rg L Fi Ri T;
+x=0,y=0,z=1,w=0; 
 J = [(1-2*x)*(-C1-V*a+V*b+2*V*a*y-V*b*y-Fa*y*z) x*(1-x)*(2*V*a-V*b-Fa*z) x*(1-x)*(-Fa*y) 0;
     y*(1-y)*(Rd-2*V*a+V*b) (1-2*y)*(-C2-Rd+V*a+Rd*x-2*V*a*x+V*b*x) 0 0;
     z*(1-z)*(Fa*y+L*y+Fi*w*y+Ri*w*y) z*(1-z)*(- Fa- L- Fi*w+ Fa*x+ L*x- Ri*w+ Fi*w*x+ Ri*w*x) (1-2*z)*(Fa-C3+L+Fi*w - Fa*y - L*y + Ri*w - Fi*w*y + Fa*x*y + L*x*y - Ri*w*y + Fi*w*x*y + Ri*w*x*y) z*(1-z)*(Fi+Ri- Fi*y - Ri*y+ Fi*x*y+Ri*x*y)
-    w*(1-w)*(C4 - Fi - T + Fi*z + Ri*z + T*z)*(-y) w*(1-w)*(C4 - Fi - T + Fi*z + Ri*z + T*z)*(1-x) w*(1-w)*(y-1-x*y)*(Fi + Ri + T) (1-2*w)*(y-1-x*y)*(C4 - Fi - T + Fi*z + Ri*z + T*z)];%雅克比矩阵
+    w*(1-w)*(C4 - Fi - T + Fi*z + Ri*z + T*z)*(-y) w*(1-w)*(C4 - Fi - T + Fi*z + Ri*z + T*z)*(1-x) w*(1-w)*(y-1-x*y)*(Fi + Ri + T) (1-2*w)*(y-1-x*y)*(C4 - Fi - T + Fi*z + Ri*z + T*z)];
 [V,R] = eig(J),DA=det(J),IA=inv(J),
-%均衡点E5（找R）
+
 clc;clear;
-syms x y z w C1 C2 C3 C4 V a b E1 E2 Fa Rd Rg L Fi Ri T;%加粗变量自行更改
-x=0,y=0,z=0,w=1; %均衡点E5
+syms x y z w C1 C2 C3 C4 V a b E1 E2 Fa Rd Rg L Fi Ri T;
+x=0,y=0,z=0,w=1; 
 J = [(1-2*x)*(-C1-V*a+V*b+2*V*a*y-V*b*y-Fa*y*z) x*(1-x)*(2*V*a-V*b-Fa*z) x*(1-x)*(-Fa*y) 0;
     y*(1-y)*(Rd-2*V*a+V*b) (1-2*y)*(-C2-Rd+V*a+Rd*x-2*V*a*x+V*b*x) 0 0;
     z*(1-z)*(Fa*y+L*y+Fi*w*y+Ri*w*y) z*(1-z)*(- Fa- L- Fi*w+ Fa*x+ L*x- Ri*w+ Fi*w*x+ Ri*w*x) (1-2*z)*(Fa-C3+L+Fi*w - Fa*y - L*y + Ri*w - Fi*w*y + Fa*x*y + L*x*y - Ri*w*y + Fi*w*x*y + Ri*w*x*y) z*(1-z)*(Fi+Ri- Fi*y - Ri*y+ Fi*x*y+Ri*x*y)
-    w*(1-w)*(C4 - Fi - T + Fi*z + Ri*z + T*z)*(-y) w*(1-w)*(C4 - Fi - T + Fi*z + Ri*z + T*z)*(1-x) w*(1-w)*(y-1-x*y)*(Fi + Ri + T) (1-2*w)*(y-1-x*y)*(C4 - Fi - T + Fi*z + Ri*z + T*z)];%雅克比矩阵
+    w*(1-w)*(C4 - Fi - T + Fi*z + Ri*z + T*z)*(-y) w*(1-w)*(C4 - Fi - T + Fi*z + Ri*z + T*z)*(1-x) w*(1-w)*(y-1-x*y)*(Fi + Ri + T) (1-2*w)*(y-1-x*y)*(C4 - Fi - T + Fi*z + Ri*z + T*z)];
 [V,R] = eig(J),DA=det(J),IA=inv(J),
-%均衡点E6（找R）
+
 clc;clear;
-syms x y z w C1 C2 C3 C4 V a b E1 E2 Fa Rd Rg L Fi Ri T;%加粗变量自行更改
-x=1,y=1,z=0,w=0; %均衡点E6
+syms x y z w C1 C2 C3 C4 V a b E1 E2 Fa Rd Rg L Fi Ri T;
+x=1,y=1,z=0,w=0; 
 J = [(1-2*x)*(-C1-V*a+V*b+2*V*a*y-V*b*y-Fa*y*z) x*(1-x)*(2*V*a-V*b-Fa*z) x*(1-x)*(-Fa*y) 0;
     y*(1-y)*(Rd-2*V*a+V*b) (1-2*y)*(-C2-Rd+V*a+Rd*x-2*V*a*x+V*b*x) 0 0;
     z*(1-z)*(Fa*y+L*y+Fi*w*y+Ri*w*y) z*(1-z)*(- Fa- L- Fi*w+ Fa*x+ L*x- Ri*w+ Fi*w*x+ Ri*w*x) (1-2*z)*(Fa-C3+L+Fi*w - Fa*y - L*y + Ri*w - Fi*w*y + Fa*x*y + L*x*y - Ri*w*y + Fi*w*x*y + Ri*w*x*y) z*(1-z)*(Fi+Ri- Fi*y - Ri*y+ Fi*x*y+Ri*x*y)
-    w*(1-w)*(C4 - Fi - T + Fi*z + Ri*z + T*z)*(-y) w*(1-w)*(C4 - Fi - T + Fi*z + Ri*z + T*z)*(1-x) w*(1-w)*(y-1-x*y)*(Fi + Ri + T) (1-2*w)*(y-1-x*y)*(C4 - Fi - T + Fi*z + Ri*z + T*z)];%雅克比矩阵
+    w*(1-w)*(C4 - Fi - T + Fi*z + Ri*z + T*z)*(-y) w*(1-w)*(C4 - Fi - T + Fi*z + Ri*z + T*z)*(1-x) w*(1-w)*(y-1-x*y)*(Fi + Ri + T) (1-2*w)*(y-1-x*y)*(C4 - Fi - T + Fi*z + Ri*z + T*z)];
 [V,R] = eig(J),DA=det(J),IA=inv(J),
-%均衡点E7（找R）
+
 clc;clear;
-syms x y z w C1 C2 C3 C4 V a b E1 E2 Fa Rd Rg L Fi Ri T;%加粗变量自行更改
-x=1,y=0,z=1,w=0; %均衡点E7
+syms x y z w C1 C2 C3 C4 V a b E1 E2 Fa Rd Rg L Fi Ri T;
+x=1,y=0,z=1,w=0; 
 J = [(1-2*x)*(-C1-V*a+V*b+2*V*a*y-V*b*y-Fa*y*z) x*(1-x)*(2*V*a-V*b-Fa*z) x*(1-x)*(-Fa*y) 0;
     y*(1-y)*(Rd-2*V*a+V*b) (1-2*y)*(-C2-Rd+V*a+Rd*x-2*V*a*x+V*b*x) 0 0;
     z*(1-z)*(Fa*y+L*y+Fi*w*y+Ri*w*y) z*(1-z)*(- Fa- L- Fi*w+ Fa*x+ L*x- Ri*w+ Fi*w*x+ Ri*w*x) (1-2*z)*(Fa-C3+L+Fi*w - Fa*y - L*y + Ri*w - Fi*w*y + Fa*x*y + L*x*y - Ri*w*y + Fi*w*x*y + Ri*w*x*y) z*(1-z)*(Fi+Ri- Fi*y - Ri*y+ Fi*x*y+Ri*x*y)
-    w*(1-w)*(C4 - Fi - T + Fi*z + Ri*z + T*z)*(-y) w*(1-w)*(C4 - Fi - T + Fi*z + Ri*z + T*z)*(1-x) w*(1-w)*(y-1-x*y)*(Fi + Ri + T) (1-2*w)*(y-1-x*y)*(C4 - Fi - T + Fi*z + Ri*z + T*z)];%雅克比矩阵
+    w*(1-w)*(C4 - Fi - T + Fi*z + Ri*z + T*z)*(-y) w*(1-w)*(C4 - Fi - T + Fi*z + Ri*z + T*z)*(1-x) w*(1-w)*(y-1-x*y)*(Fi + Ri + T) (1-2*w)*(y-1-x*y)*(C4 - Fi - T + Fi*z + Ri*z + T*z)];
 [V,R] = eig(J),DA=det(J),IA=inv(J),
-%均衡点E8（找R）
+
 clc;clear;
-syms x y z w C1 C2 C3 C4 V a b E1 E2 Fa Rd Rg L Fi Ri T;%加粗变量自行更改
-x=1,y=0,z=0,w=1; %均衡点E8
+syms x y z w C1 C2 C3 C4 V a b E1 E2 Fa Rd Rg L Fi Ri T;
+x=1,y=0,z=0,w=1; 
 J = [(1-2*x)*(-C1-V*a+V*b+2*V*a*y-V*b*y-Fa*y*z) x*(1-x)*(2*V*a-V*b-Fa*z) x*(1-x)*(-Fa*y) 0;
     y*(1-y)*(Rd-2*V*a+V*b) (1-2*y)*(-C2-Rd+V*a+Rd*x-2*V*a*x+V*b*x) 0 0;
     z*(1-z)*(Fa*y+L*y+Fi*w*y+Ri*w*y) z*(1-z)*(- Fa- L- Fi*w+ Fa*x+ L*x- Ri*w+ Fi*w*x+ Ri*w*x) (1-2*z)*(Fa-C3+L+Fi*w - Fa*y - L*y + Ri*w - Fi*w*y + Fa*x*y + L*x*y - Ri*w*y + Fi*w*x*y + Ri*w*x*y) z*(1-z)*(Fi+Ri- Fi*y - Ri*y+ Fi*x*y+Ri*x*y)
-    w*(1-w)*(C4 - Fi - T + Fi*z + Ri*z + T*z)*(-y) w*(1-w)*(C4 - Fi - T + Fi*z + Ri*z + T*z)*(1-x) w*(1-w)*(y-1-x*y)*(Fi + Ri + T) (1-2*w)*(y-1-x*y)*(C4 - Fi - T + Fi*z + Ri*z + T*z)];%雅克比矩阵
+    w*(1-w)*(C4 - Fi - T + Fi*z + Ri*z + T*z)*(-y) w*(1-w)*(C4 - Fi - T + Fi*z + Ri*z + T*z)*(1-x) w*(1-w)*(y-1-x*y)*(Fi + Ri + T) (1-2*w)*(y-1-x*y)*(C4 - Fi - T + Fi*z + Ri*z + T*z)];
 [V,R] = eig(J),DA=det(J),IA=inv(J),
-%均衡点E9（找R）
+
 clc;clear;
-syms x y z w C1 C2 C3 C4 V a b E1 E2 Fa Rd Rg L Fi Ri T;%加粗变量自行更改
-x=0,y=1,z=1,w=0; %均衡点E9
+syms x y z w C1 C2 C3 C4 V a b E1 E2 Fa Rd Rg L Fi Ri T;
+x=0,y=1,z=1,w=0; 
 J = [(1-2*x)*(-C1-V*a+V*b+2*V*a*y-V*b*y-Fa*y*z) x*(1-x)*(2*V*a-V*b-Fa*z) x*(1-x)*(-Fa*y) 0;
     y*(1-y)*(Rd-2*V*a+V*b) (1-2*y)*(-C2-Rd+V*a+Rd*x-2*V*a*x+V*b*x) 0 0;
     z*(1-z)*(Fa*y+L*y+Fi*w*y+Ri*w*y) z*(1-z)*(- Fa- L- Fi*w+ Fa*x+ L*x- Ri*w+ Fi*w*x+ Ri*w*x) (1-2*z)*(Fa-C3+L+Fi*w - Fa*y - L*y + Ri*w - Fi*w*y + Fa*x*y + L*x*y - Ri*w*y + Fi*w*x*y + Ri*w*x*y) z*(1-z)*(Fi+Ri- Fi*y - Ri*y+ Fi*x*y+Ri*x*y)
-    w*(1-w)*(C4 - Fi - T + Fi*z + Ri*z + T*z)*(-y) w*(1-w)*(C4 - Fi - T + Fi*z + Ri*z + T*z)*(1-x) w*(1-w)*(y-1-x*y)*(Fi + Ri + T) (1-2*w)*(y-1-x*y)*(C4 - Fi - T + Fi*z + Ri*z + T*z)];%雅克比矩阵
+    w*(1-w)*(C4 - Fi - T + Fi*z + Ri*z + T*z)*(-y) w*(1-w)*(C4 - Fi - T + Fi*z + Ri*z + T*z)*(1-x) w*(1-w)*(y-1-x*y)*(Fi + Ri + T) (1-2*w)*(y-1-x*y)*(C4 - Fi - T + Fi*z + Ri*z + T*z)];% 
 [V,R] = eig(J),DA=det(J),IA=inv(J),
-%均衡点E10（找R）
+
 clc;clear;
-syms x y z w C1 C2 C3 C4 V a b E1 E2 Fa Rd Rg L Fi Ri T;%加粗变量自行更改
-x=0,y=1,z=0,w=1; %均衡点E10
+syms x y z w C1 C2 C3 C4 V a b E1 E2 Fa Rd Rg L Fi Ri T; 
+x=0,y=1,z=0,w=1; 
 J = [(1-2*x)*(-C1-V*a+V*b+2*V*a*y-V*b*y-Fa*y*z) x*(1-x)*(2*V*a-V*b-Fa*z) x*(1-x)*(-Fa*y) 0;
     y*(1-y)*(Rd-2*V*a+V*b) (1-2*y)*(-C2-Rd+V*a+Rd*x-2*V*a*x+V*b*x) 0 0;
     z*(1-z)*(Fa*y+L*y+Fi*w*y+Ri*w*y) z*(1-z)*(- Fa- L- Fi*w+ Fa*x+ L*x- Ri*w+ Fi*w*x+ Ri*w*x) (1-2*z)*(Fa-C3+L+Fi*w - Fa*y - L*y + Ri*w - Fi*w*y + Fa*x*y + L*x*y - Ri*w*y + Fi*w*x*y + Ri*w*x*y) z*(1-z)*(Fi+Ri- Fi*y - Ri*y+ Fi*x*y+Ri*x*y)
-    w*(1-w)*(C4 - Fi - T + Fi*z + Ri*z + T*z)*(-y) w*(1-w)*(C4 - Fi - T + Fi*z + Ri*z + T*z)*(1-x) w*(1-w)*(y-1-x*y)*(Fi + Ri + T) (1-2*w)*(y-1-x*y)*(C4 - Fi - T + Fi*z + Ri*z + T*z)];%雅克比矩阵
+    w*(1-w)*(C4 - Fi - T + Fi*z + Ri*z + T*z)*(-y) w*(1-w)*(C4 - Fi - T + Fi*z + Ri*z + T*z)*(1-x) w*(1-w)*(y-1-x*y)*(Fi + Ri + T) (1-2*w)*(y-1-x*y)*(C4 - Fi - T + Fi*z + Ri*z + T*z)];% 
 [V,R] = eig(J),DA=det(J),IA=inv(J),
-%均衡点E11（找R）
+
 clc;clear;
-syms x y z w C1 C2 C3 C4 V a b E1 E2 Fa Rd Rg L Fi Ri T;%加粗变量自行更改
-x=0,y=0,z=1,w=1; %均衡点E11
+syms x y z w C1 C2 C3 C4 V a b E1 E2 Fa Rd Rg L Fi Ri T; 
+x=0,y=0,z=1,w=1; 
 J = [(1-2*x)*(-C1-V*a+V*b+2*V*a*y-V*b*y-Fa*y*z) x*(1-x)*(2*V*a-V*b-Fa*z) x*(1-x)*(-Fa*y) 0;
     y*(1-y)*(Rd-2*V*a+V*b) (1-2*y)*(-C2-Rd+V*a+Rd*x-2*V*a*x+V*b*x) 0 0;
     z*(1-z)*(Fa*y+L*y+Fi*w*y+Ri*w*y) z*(1-z)*(- Fa- L- Fi*w+ Fa*x+ L*x- Ri*w+ Fi*w*x+ Ri*w*x) (1-2*z)*(Fa-C3+L+Fi*w - Fa*y - L*y + Ri*w - Fi*w*y + Fa*x*y + L*x*y - Ri*w*y + Fi*w*x*y + Ri*w*x*y) z*(1-z)*(Fi+Ri- Fi*y - Ri*y+ Fi*x*y+Ri*x*y)
-    w*(1-w)*(C4 - Fi - T + Fi*z + Ri*z + T*z)*(-y) w*(1-w)*(C4 - Fi - T + Fi*z + Ri*z + T*z)*(1-x) w*(1-w)*(y-1-x*y)*(Fi + Ri + T) (1-2*w)*(y-1-x*y)*(C4 - Fi - T + Fi*z + Ri*z + T*z)];%雅克比矩阵
+    w*(1-w)*(C4 - Fi - T + Fi*z + Ri*z + T*z)*(-y) w*(1-w)*(C4 - Fi - T + Fi*z + Ri*z + T*z)*(1-x) w*(1-w)*(y-1-x*y)*(Fi + Ri + T) (1-2*w)*(y-1-x*y)*(C4 - Fi - T + Fi*z + Ri*z + T*z)];% 
 [V,R] = eig(J),DA=det(J),IA=inv(J),
-%均衡点E12（找R）
+
 clc;clear;
-syms x y z w C1 C2 C3 C4 V a b E1 E2 Fa Rd Rg L Fi Ri T;%加粗变量自行更改
-x=0,y=1,z=1,w=1; %均衡点E12
+syms x y z w C1 C2 C3 C4 V a b E1 E2 Fa Rd Rg L Fi Ri T; 
+x=0,y=1,z=1,w=1; 
 J = [(1-2*x)*(-C1-V*a+V*b+2*V*a*y-V*b*y-Fa*y*z) x*(1-x)*(2*V*a-V*b-Fa*z) x*(1-x)*(-Fa*y) 0;
     y*(1-y)*(Rd-2*V*a+V*b) (1-2*y)*(-C2-Rd+V*a+Rd*x-2*V*a*x+V*b*x) 0 0;
     z*(1-z)*(Fa*y+L*y+Fi*w*y+Ri*w*y) z*(1-z)*(- Fa- L- Fi*w+ Fa*x+ L*x- Ri*w+ Fi*w*x+ Ri*w*x) (1-2*z)*(Fa-C3+L+Fi*w - Fa*y - L*y + Ri*w - Fi*w*y + Fa*x*y + L*x*y - Ri*w*y + Fi*w*x*y + Ri*w*x*y) z*(1-z)*(Fi+Ri- Fi*y - Ri*y+ Fi*x*y+Ri*x*y)
-    w*(1-w)*(C4 - Fi - T + Fi*z + Ri*z + T*z)*(-y) w*(1-w)*(C4 - Fi - T + Fi*z + Ri*z + T*z)*(1-x) w*(1-w)*(y-1-x*y)*(Fi + Ri + T) (1-2*w)*(y-1-x*y)*(C4 - Fi - T + Fi*z + Ri*z + T*z)];%雅克比矩阵
+    w*(1-w)*(C4 - Fi - T + Fi*z + Ri*z + T*z)*(-y) w*(1-w)*(C4 - Fi - T + Fi*z + Ri*z + T*z)*(1-x) w*(1-w)*(y-1-x*y)*(Fi + Ri + T) (1-2*w)*(y-1-x*y)*(C4 - Fi - T + Fi*z + Ri*z + T*z)];% 
 [V,R] = eig(J),DA=det(J),IA=inv(J),
-%均衡点E13（找R）
+
 clc;clear;
-syms x y z w C1 C2 C3 C4 V a b E1 E2 Fa Rd Rg L Fi Ri T;%加粗变量自行更改
-x=1,y=0,z=1,w=1; %均衡点E13
+syms x y z w C1 C2 C3 C4 V a b E1 E2 Fa Rd Rg L Fi Ri T; 
+x=1,y=0,z=1,w=1; 
 J = [(1-2*x)*(-C1-V*a+V*b+2*V*a*y-V*b*y-Fa*y*z) x*(1-x)*(2*V*a-V*b-Fa*z) x*(1-x)*(-Fa*y) 0;
     y*(1-y)*(Rd-2*V*a+V*b) (1-2*y)*(-C2-Rd+V*a+Rd*x-2*V*a*x+V*b*x) 0 0;
     z*(1-z)*(Fa*y+L*y+Fi*w*y+Ri*w*y) z*(1-z)*(- Fa- L- Fi*w+ Fa*x+ L*x- Ri*w+ Fi*w*x+ Ri*w*x) (1-2*z)*(Fa-C3+L+Fi*w - Fa*y - L*y + Ri*w - Fi*w*y + Fa*x*y + L*x*y - Ri*w*y + Fi*w*x*y + Ri*w*x*y) z*(1-z)*(Fi+Ri- Fi*y - Ri*y+ Fi*x*y+Ri*x*y)
-    w*(1-w)*(C4 - Fi - T + Fi*z + Ri*z + T*z)*(-y) w*(1-w)*(C4 - Fi - T + Fi*z + Ri*z + T*z)*(1-x) w*(1-w)*(y-1-x*y)*(Fi + Ri + T) (1-2*w)*(y-1-x*y)*(C4 - Fi - T + Fi*z + Ri*z + T*z)];%雅克比矩阵
+    w*(1-w)*(C4 - Fi - T + Fi*z + Ri*z + T*z)*(-y) w*(1-w)*(C4 - Fi - T + Fi*z + Ri*z + T*z)*(1-x) w*(1-w)*(y-1-x*y)*(Fi + Ri + T) (1-2*w)*(y-1-x*y)*(C4 - Fi - T + Fi*z + Ri*z + T*z)];% 
 [V,R] = eig(J),DA=det(J),IA=inv(J),
-%均衡点E14（找R）
+
 clc;clear;
-syms x y z w C1 C2 C3 C4 V a b E1 E2 Fa Rd Rg L Fi Ri T;%加粗变量自行更改
-x=1,y=1,z=0,w=1; %均衡点E14
+syms x y z w C1 C2 C3 C4 V a b E1 E2 Fa Rd Rg L Fi Ri T; 
+x=1,y=1,z=0,w=1; 
 J = [(1-2*x)*(-C1-V*a+V*b+2*V*a*y-V*b*y-Fa*y*z) x*(1-x)*(2*V*a-V*b-Fa*z) x*(1-x)*(-Fa*y) 0;
     y*(1-y)*(Rd-2*V*a+V*b) (1-2*y)*(-C2-Rd+V*a+Rd*x-2*V*a*x+V*b*x) 0 0;
     z*(1-z)*(Fa*y+L*y+Fi*w*y+Ri*w*y) z*(1-z)*(- Fa- L- Fi*w+ Fa*x+ L*x- Ri*w+ Fi*w*x+ Ri*w*x) (1-2*z)*(Fa-C3+L+Fi*w - Fa*y - L*y + Ri*w - Fi*w*y + Fa*x*y + L*x*y - Ri*w*y + Fi*w*x*y + Ri*w*x*y) z*(1-z)*(Fi+Ri- Fi*y - Ri*y+ Fi*x*y+Ri*x*y)
-    w*(1-w)*(C4 - Fi - T + Fi*z + Ri*z + T*z)*(-y) w*(1-w)*(C4 - Fi - T + Fi*z + Ri*z + T*z)*(1-x) w*(1-w)*(y-1-x*y)*(Fi + Ri + T) (1-2*w)*(y-1-x*y)*(C4 - Fi - T + Fi*z + Ri*z + T*z)];%雅克比矩阵
+    w*(1-w)*(C4 - Fi - T + Fi*z + Ri*z + T*z)*(-y) w*(1-w)*(C4 - Fi - T + Fi*z + Ri*z + T*z)*(1-x) w*(1-w)*(y-1-x*y)*(Fi + Ri + T) (1-2*w)*(y-1-x*y)*(C4 - Fi - T + Fi*z + Ri*z + T*z)];% 
 [V,R] = eig(J),DA=det(J),IA=inv(J),
-%均衡点E15（找R）
+
 clc;clear;
-syms x y z w C1 C2 C3 C4 V a b E1 E2 Fa Rd Rg L Fi Ri T;%加粗变量自行更改
-x=1,y=1,z=1,w=0; %均衡点E15
+syms x y z w C1 C2 C3 C4 V a b E1 E2 Fa Rd Rg L Fi Ri T; 
+x=1,y=1,z=1,w=0; 
 J = [(1-2*x)*(-C1-V*a+V*b+2*V*a*y-V*b*y-Fa*y*z) x*(1-x)*(2*V*a-V*b-Fa*z) x*(1-x)*(-Fa*y) 0;
     y*(1-y)*(Rd-2*V*a+V*b) (1-2*y)*(-C2-Rd+V*a+Rd*x-2*V*a*x+V*b*x) 0 0;
     z*(1-z)*(Fa*y+L*y+Fi*w*y+Ri*w*y) z*(1-z)*(- Fa- L- Fi*w+ Fa*x+ L*x- Ri*w+ Fi*w*x+ Ri*w*x) (1-2*z)*(Fa-C3+L+Fi*w - Fa*y - L*y + Ri*w - Fi*w*y + Fa*x*y + L*x*y - Ri*w*y + Fi*w*x*y + Ri*w*x*y) z*(1-z)*(Fi+Ri- Fi*y - Ri*y+ Fi*x*y+Ri*x*y)
-    w*(1-w)*(C4 - Fi - T + Fi*z + Ri*z + T*z)*(-y) w*(1-w)*(C4 - Fi - T + Fi*z + Ri*z + T*z)*(1-x) w*(1-w)*(y-1-x*y)*(Fi + Ri + T) (1-2*w)*(y-1-x*y)*(C4 - Fi - T + Fi*z + Ri*z + T*z)];%雅克比矩阵
+    w*(1-w)*(C4 - Fi - T + Fi*z + Ri*z + T*z)*(-y) w*(1-w)*(C4 - Fi - T + Fi*z + Ri*z + T*z)*(1-x) w*(1-w)*(y-1-x*y)*(Fi + Ri + T) (1-2*w)*(y-1-x*y)*(C4 - Fi - T + Fi*z + Ri*z + T*z)];% 
 [V,R] = eig(J),DA=det(J),IA=inv(J),
-%均衡点E16（找R）
+
 clc;clear;
-syms x y z w C1 C2 C3 C4 V a b E1 E2 Fa Rd Rg L Fi Ri T;%加粗变量自行更改
-x=1,y=1,z=1,w=1; %均衡点E16
+syms x y z w C1 C2 C3 C4 V a b E1 E2 Fa Rd Rg L Fi Ri T; 
+x=1,y=1,z=1,w=1; 
 J = [(1-2*x)*(-C1-V*a+V*b+2*V*a*y-V*b*y-Fa*y*z) x*(1-x)*(2*V*a-V*b-Fa*z) x*(1-x)*(-Fa*y) 0;
     y*(1-y)*(Rd-2*V*a+V*b) (1-2*y)*(-C2-Rd+V*a+Rd*x-2*V*a*x+V*b*x) 0 0;
     z*(1-z)*(Fa*y+L*y+Fi*w*y+Ri*w*y) z*(1-z)*(- Fa- L- Fi*w+ Fa*x+ L*x- Ri*w+ Fi*w*x+ Ri*w*x) (1-2*z)*(Fa-C3+L+Fi*w - Fa*y - L*y + Ri*w - Fi*w*y + Fa*x*y + L*x*y - Ri*w*y + Fi*w*x*y + Ri*w*x*y) z*(1-z)*(Fi+Ri- Fi*y - Ri*y+ Fi*x*y+Ri*x*y)
-    w*(1-w)*(C4 - Fi - T + Fi*z + Ri*z + T*z)*(-y) w*(1-w)*(C4 - Fi - T + Fi*z + Ri*z + T*z)*(1-x) w*(1-w)*(y-1-x*y)*(Fi + Ri + T) (1-2*w)*(y-1-x*y)*(C4 - Fi - T + Fi*z + Ri*z + T*z)];%雅克比矩阵
+    w*(1-w)*(C4 - Fi - T + Fi*z + Ri*z + T*z)*(-y) w*(1-w)*(C4 - Fi - T + Fi*z + Ri*z + T*z)*(1-x) w*(1-w)*(y-1-x*y)*(Fi + Ri + T) (1-2*w)*(y-1-x*y)*(C4 - Fi - T + Fi*z + Ri*z + T*z)];% 
 [V,R] = eig(J),DA=det(J),IA=inv(J),
